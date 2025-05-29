be881b49dee71c3f6b967788257e380155699a5d
aditya2145 <adityasharma05986.com>
2025-05-29 09:28:15 +0530

    initial commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,14 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react(), tailwindcss()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      }
-    }
-  },
 })
